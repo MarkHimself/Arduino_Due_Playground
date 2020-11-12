b131704e4874e279e92c912b04579a647e80ba4c
Mark <mark.prudnikov@wsu.edu>
2020-11-11 19:16:36 -0800

    Analog Read example

--- src/main.cpp
+++ src/main.cpp
@@ -18,15 +18,19 @@
 - DMA
 */
 
-/* Set up reading digital input on pin 40
-D.P. 40 = PC8
+/* Set up reading analog input on pin A7
+Arduino Analog Pin A7 = PIOA2 (PIOA2 if/when used as a digital pin)
 Ch. 31 is PIO Controller
+Ch. 43 is ADC Controller
+Peripheral: X1 (extra function)
+Signal: AD0
+Channel: 0 		pg. 1326
+ADC is 12 bits (from 0 = ground to 4095 = 3.3V)
 */
 
-void PC8_Input_Mode();
-void PC8_Pullup_Enable();
-void PC8_Pullup_Disable();
-bool PC8_Input_State();
+void reset_ADC();
+void PIOA2_Analog_Input_Mode();
+uint16_t PIOA2_Input_State();
 
 uint32_t b[32];
 
@@ -34,41 +38,55 @@ uint32_t b[32];
 void setup() {
 	for (uint32_t i = 0; i < 32; i++) b[i] = (1u << i);
 	Serial.begin(9600);
-	PC8_Input_Mode();
-	PC8_Pullup_Enable();
+	reset_ADC();
+	PIOA2_Analog_Input_Mode();
 }
 
 void loop() {
 	
-	if (PC8_Input_State() == true){
-		Serial.println("ON");
-	}
-	else {
-		Serial.println("OFF");
-	}
+	uint16_t adc_val = 0;
+	adc_val = PIOA2_Input_State();
+	
+	Serial.println(adc_val);
 	delay(100);
 }
 
-void PC8_Input_Mode(){
+void reset_ADC(){
+	ADC->ADC_CR = b[0];						// reset ADC						pg. 1332
+	ADC->ADC_WPSR;							// clear any flags					pg. 1354
+	ADC->ADC_WPMR = (0x414443 << 8);		// disable write protect			pg. 1353
+	ADC->ADC_MR = 0
+		| (4 << 8)							// ADC clock prescale
+		//| (2 << 16)							// startup time is 16 periods of ADC clock
+		| (6 << 16)							// startup time is 96 periods of ADC clock
+		//| (2 << 20)							// settling time is 9 periods of ADC clock
+		| (3 << 20)							// settling time is 17 periods of ADC clock
+	;
+}
+
+void PIOA2_Analog_Input_Mode(){
 	// pin multiplexing
-	PIOC->PIO_PER = b[8];				// pin controlled by PIO controller
-	PIOC->PIO_ODR = b[8];				// output disable register
-	PIOC->PIO_IFDR = b[8];				// disable input filter
+	ADC->ADC_CHER = b[0];			// ADC_CHER - CHannel Enable Register pg. 1338
+	// remove control from PIO (done with ADC_CHER? see pg. 1319)
+	
+	// single ended mode pg. 1326
+	
 	
 	// power management
-	PMC->PMC_PCER0 = b[13];					// enable clock for PIOC.			pg. 542, 38
+	PMC->PMC_PCER0 = b[11];					// enable clock for PIOA			pg. 
+	PMC->PMC_PCER1 = b[5];					// enable clock for PIOC.			pg. 39, 1319, 563
 }
 
-void PC8_Pullup_Enable(){
-	PIOC->PIO_PUER = b[8];
-}
 
-void PC8_Pullup_Disable(){
-	PIOC->PIO_PUDR = b[8];
-}
-
-bool PC8_Input_State(){
-	return PIOC->PIO_PDSR & b[8];		// return status of pin
+uint16_t PIOA2_Input_State(){
+	ADC->ADC_CR = b[1];						// start conversion					pg. 1332
+	
+	while (!(ADC->ADC_ISR & b[0])){};		// wait for conversion to finish	pg. 1345
+	// this is ok since only have 1 adc channel.
+	// better to have a class that checks if any finished (through the DRDY bit)
+	// and then update whatever what finished.
+	
+	return ADC->ADC_CDR[0];					// return the data					pg. 
 }
 
 
